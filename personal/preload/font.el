8be74f4729db63bab7964430fe993f2d489ab943
Andrei Veprev <andrei.veprev@objectstyle.com>
2019-11-28 14:35:31 +0300

    Customizing font

---
+++ personal/preload/font.el
@@ -0,0 +1,4 @@
+;; Use the following to get full name for a font
+;; M-x ns-popup-font-panel
+;; M-x describe-font
+(set-default-font "-*-Iosevka Nerd Font-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
