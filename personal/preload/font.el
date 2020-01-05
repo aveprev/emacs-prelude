9b96aac3b18d476024dc316f1fca61d7dc0fbd1f
Andrei Veprev <andrei.veprev@gmail.com>
2020-01-05 13:49:55 +0300

    Font customizations for differents systems

--- personal/preload/font.el
+++ personal/preload/font.el
@@ -1,4 +1,8 @@
 ;; Use the following to get full name for a font
 ;; M-x ns-popup-font-panel
 ;; M-x describe-font
-(set-default-font "-*-Iosevka Nerd Font-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+(cond
+ ((eq system-type 'darwin)
+  (set-default-font "-*-Iosevka Nerd Font-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"))
+ ((eq system-type 'gnu/linux)
+  (set-default-font "-*-Iosevka Nerd Font-normal-normal-normal-*-28-*-*-*-m-0-iso10646-1")))
